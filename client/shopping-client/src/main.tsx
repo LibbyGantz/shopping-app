ce3c6406f9455367008be64c529ee84177173b66
Libby Gantz <libigantz@gmail.com>
2025-07-24 00:25:05 +0300

    tailwindcss is installed properly

--- client/shopping-client/src/main.tsx
+++ client/shopping-client/src/main.tsx
@@ -10,9 +10,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
-      {/* <button className="px-6 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600 transition">
-  לחצן ניסיון
-</button> */}
     </Provider>
   </React.StrictMode>,
 );
