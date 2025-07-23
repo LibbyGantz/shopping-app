df4ff729da06adc0df37884cc5eef355622e96f0
Libby Gantz <libigantz@gmail.com>
2025-07-23 21:39:05 +0300

    remove tailwind.css

--- client/shopping-client/src/main.tsx
+++ client/shopping-client/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-//import './index.css';
+import './index.css';
 
 import { Provider } from 'react-redux';
 import { store } from './app/store';
@@ -10,6 +10,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
+      {/* <button className="px-6 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600 transition">
+  לחצן ניסיון
+</button> */}
     </Provider>
   </React.StrictMode>,
 );
