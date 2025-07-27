e534cbe16933dc1813c326ccda6a027471779ec6
Libby Gantz <libigantz@gmail.com>
2025-07-27 10:16:59 +0300

    .

--- server-dotnet/ShoppingApi/obj/Debug/net8.0/ShoppingApi.AssemblyInfo.cs
+++ server-dotnet/ShoppingApi/obj/Debug/net8.0/ShoppingApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShoppingApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f87829ae7ea72cf8f4b35b132b1c7b5c091bbc72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47f075ad6e89224adcfbfb153ce41901116ac203")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShoppingApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShoppingApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
